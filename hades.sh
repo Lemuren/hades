73e3ece23e4e98b14bcdb047f3f397fb294234ef
Lemuren <h.e.thorsell@gmail.com>
2023-11-13 01:21:51 +0100

    Change binding for quit program and clean up after script

--- hades.sh
+++ hades.sh
@@ -3,7 +3,7 @@
 sudo echo "Echoing with sudo to get privileges for the rest of the script"
 
 echo "Running system update"
-sudo apt update && sudo apt upgrade --yes
+sudo apt update && sudo apt upgrade --yes 1> /dev/null
 
 echo "Making directories"
 mkdir "$HOME/.local/bin"
@@ -16,10 +16,10 @@ echo "Removing bash_logout"
 rm "$HOME/.bash_logout"
 
 echo "Installing software"
-sudo apt install --yes firefox-esr neovim git xorg x11-xkb-utils zathura htop mpv libxft-dev libxinerama-dev
+sudo apt install --yes firefox-esr neovim git xorg x11-xkb-utils zathura htop mpv libxft-dev libxinerama-dev 1> /dev/null
 
 echo "Cloning repo"
-git clone https://github.com/Lemuren/hades.git
+git clone https://github.com/Lemuren/hades.git 1> /dev/null
 cd hades
 
 echo "Building st"
@@ -41,3 +41,6 @@ echo "Copying dotfiles"
 cp dotfiles/.xinitrc $HOME
 cp dotfiles/.profile $HOME
 cp dotfiles/.bashrc $HOME
+
+echo "Cleaning up"
+rm -rf hades hades.sh
