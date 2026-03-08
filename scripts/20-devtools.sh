0505f612ac652e90dda49ba6c93a10dcd6a408ed
Lemuren <h.e.thorsell@gmail.com>
2026-03-08 01:42:17 +0100

    Create 20-devtools.sh

---
+++ scripts/20-devtools.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+#
+# Here we list development libraries, programming languages, and other dev tools.
+#
+
+# First all the normal developer tools and languages.
+echo "==> Installing developer tools"
+apt install -y   \
+    make         \
+    gcc          \
+    g++          \
+    pkg-config   \
+    python3      \
+    python3-pip  \
+    python3-venv \
+    golang
+
+# Next Docker, which is a bit more involved.
+echo "==> Installing Docker"
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+apt update
+
+apt install -y            \
+    docker-ce             \
+    docker-ce-cli         \
+    containerd.io         \
+    docker-buildx-plugin  \
+    docker-compose-plugin
+
+usermod -aG docker "${USER}"
