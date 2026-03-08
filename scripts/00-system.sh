734a812bfebb1c2afd125492850f124f09a3b6bc
Lemuren <h.e.thorsell@gmail.com>
2026-03-08 01:41:37 +0100

    Create 00-system.sh

---
+++ scripts/00-system.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# 
+# This part of the install script is responsible for updating the system,
+# setting up the home directory, and installing some basic packages.
+#
+
+# Make sure the system is up-to-date before we do anything else.
+echo "==> Updating system"
+apt update
+apt upgrade -y
+
+# We're going to clobber these later anyway.
+rm .bash_logout
+rm .bashrc
+rm .profile
+
+# Set up our home directory.
+echo "==> Creating HOME directory layout"
+mkdir -p "${HOME}/docs"
+mkdir -p "${HOME}/repos"
+mkdir -p "${HOME}/downloads"
+mkdir -p "${HOME}/pictures"
+mkdir -p "${HOME}/.local/bin"
