e41bdb4364b2a684d532c3293223a240ba31a0cb
Lemuren <h.e.thorsell@gmail.com>
2026-03-08 02:01:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Installing HaDeS"
+# We 'sudo' echo here to have sudo access later when we need it.
+sudo echo "Installing HaDeS"
+
 for script in scripts/*.sh; do
     echo "======== ${script} ========"
     if ! bash "${script}"; then
