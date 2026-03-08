03faad0df3946503ab9efd3d86e47d4410754f3f
Lemuren <h.e.thorsell@gmail.com>
2026-03-08 01:41:53 +0100

    Create 10-packages.sh

---
+++ scripts/10-packages.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+#
+# Here we list all the (non-developer) packages we want to install.
+#
+
+echo "==> Installing basic packages"
+apt install -y           \
+    git                  \
+    curl                 \
+    build-essential      \
+    python-is-python3    \
+    htop                 \
+    neovim               \
+    x11-xkb-utils        \
+    suckless-tools       \
+    unzip                \
+    ca-certificates      \
+    gnupg                \
+    zip                  \
+    xz-utils
